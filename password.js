1702f368c91a34000ec23eb72a1b6d122e6f9904
SRK SHADHIN <mt6w6v6x5b@privaterelay.appleid.com>
2026-03-09 05:42:48 +0600

    Add password generator and copy to clipboard feature
    Implement password generation and copying functionality.

---
+++ password.js
@@ -0,0 +1,62 @@
+function generatePassword(){
+
+let length=document.getElementById("length").value;
+
+let useUpper=document.getElementById("uppercase").checked;
+
+let useNumbers=document.getElementById("numbers").checked;
+
+let useSymbols=document.getElementById("symbols").checked;
+
+let lower="abcdefghijklmnopqrstuvwxyz";
+
+let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let numbers="0123456789";
+
+let symbols="!@#$%^&*()_+";
+
+let chars=lower;
+
+if(useUpper){
+
+chars+=upper;
+
+}
+
+if(useNumbers){
+
+chars+=numbers;
+
+}
+
+if(useSymbols){
+
+chars+=symbols;
+
+}
+
+let password="";
+
+for(let i=0;i<length;i++){
+
+let random=Math.floor(Math.random()*chars.length);
+
+password+=chars[random];
+
+}
+
+document.getElementById("passwordResult").innerText=password;
+
+}
+
+
+function copyPassword(){
+
+let password=document.getElementById("passwordResult").innerText;
+
+navigator.clipboard.writeText(password);
+
+alert("Password Copied");
+
+}
